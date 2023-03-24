2a0ebdbc44a8895a9d04086f469a40ab5c266832
Claire Chang <claire0505@gmail.com>
2023-03-25 01:41:12 +0800

    initial commit

---
+++ english _word _quiz _game.py
@@ -0,0 +1,128 @@
+import random
+import re
+
+# 讀取英文單字檔案，沒有就建立新字典檔案
+def load_words():
+    try:
+        with open("english_word.txt", "r", encoding="utf-8") as f:
+            word_dict = eval(f.read())
+            return word_dict
+    except:
+        with open("english_word.txt", "w", encoding="utf-8") as f:
+            word_dict = {"apple": "蘋果", "banana": "香蕉",
+                         "orange": "橘子", "watermelon": "西瓜", "kiwi": "奇異果"}
+            f.write(str(word_dict))
+            return word_dict
+
+# 新增單字
+def add_word():
+    # 檢查輸入的字是否為英文字母或中文字
+    while True:
+        # 輸入英文單字
+        english_word = input("請輸入要新增的英文單字 (取消輸入按 【q 】結束):\n")
+        # 檢查是否為 q
+        if english_word == "q":
+            print("取消新增英文單字。")
+            return
+        # 檢查是否為英文字母
+        if not (english_word.isalpha() and english_word.isascii()):
+            print("輸入的不是英文字母，請重新輸入")
+            continue
+        
+        # 輸入中文單字
+        chinese_word = input("請輸入要新增的中文單字 (取消輸入按【q 】結束):\n")
+        # 檢查是否為 q
+        if chinese_word == "q":
+            print("取消新增中文單字。")
+            return
+        # 檢查是否為中文字
+        match = re.search(r"[\u4e00-\u9fff]", chinese_word)
+        if not match:
+            print("輸入的不是中文字，請重新輸入")
+            continue
+        
+        # 新增成功
+        print("新增英文單字成功!")
+        eng = english_word
+        print(eng)
+        
+        print("新增中文翻譯成功!")
+        chi = chinese_word
+        print(chi)
+        
+        # 將字典寫入檔案
+        word_dict = load_words()
+        word_dict[english_word] = chinese_word
+        with open ("english_word.txt","w", encoding="utf8") as f:
+            f.write(str(word_dict))
+        print("單字已成功儲存到檔案裡!")
+        
+        # 是否繼續新增單字
+        choice = input("是否要繼續新增單字？(y/n) \n")
+        while True:
+            if choice.lower() == "y": 
+                break # 回到迴圈開始處
+            elif choice.lower() == "n": 
+                return # 結束函式
+            else:
+                print("無效的選擇，只能選擇 y 或 n")
+
+# 猜英文單字遊戲
+def english_word_quiz():
+    print("英文單字測驗，每回測驗共有五題,每題最多有三次機會回答")
+    words = load_words()
+    score = 0
+    # 每回測驗共有五題
+    for i in range(5):
+        # 隨機選擇一個單字
+        word = random.choice(list(words.items()))
+        print(f"\n第{i+1}題,請回答正確單字的英文單字")
+        quiz = word[0]
+        # 每題最多可回答三次
+        # 初始提示字母數量為0
+        hint = 0
+        for j in range(1,4):
+            answer = input(f"請輸入【{word[1]}】的英文單字: ")
+            if answer == quiz:
+                if j == 1:
+                    print("太棒了! 一次就答對了! 可獲得20分!")
+                    score += 20
+                    break # 跳出內層迴圈，進行下一題
+                else:
+                    if j == 2:
+                        print("不錯哦，第二次就答對了! 可獲得10分")
+                        score += 10
+                        break # 跳出內層迴圈，進行下一題
+                    elif j == 3:
+                        print("還可以，第三次才答對了! 可獲得5分")
+                        score += 5
+                        break # 跳出內層迴圈，進行下一題
+            else:
+                if j < 3:
+                    hint +=1 # 增加提示字母數量
+                    print(f"不對哦!請再試試看! 提示: {quiz[0:hint]}")
+                else:
+                    print("要加油哦!答題失敗。正確答案是：", quiz)
+    
+    # 五題答完後會跳出，顯示目前得分，是否要繼續英文單字測驗
+    print(f"\n您的分數為: {score} 分")
+    choice = input("是否要繼續英文單字測驗？(y/n): ")
+    if choice.lower() == "y":
+       english_word_quiz() # 繼續執行
+    elif choice.lower() == "n":
+       return score # 結束程式並返回得分 
+
+# 主程式:選擇項目
+load_words()
+print("<<< 歡迎來到英文單字測驗遊戲，請選擇要執行的項目： >>>")
+while True:
+    choice = input("請輸入您的選擇 1.新增單字 2.英文單字測驗遊戲 3.退出 : ")
+    if choice == "1":
+        add_word() # 新增單字
+    elif choice == "2":
+        english_word_quiz() # 單字測驗
+    elif choice == "3":
+        print("退出")
+        break     
+    else:
+        print("輸入錯誤,請重新選擇: 1.新增單字 2.英文單字測驗遊戲 3.退出")
